da2c61bdb6c26fcd8e506ca1981c616fe501dbf3
Andy Nguyen <78712772+Nguye218@users.noreply.github.com>
2021-12-30 11:06:01 -0800

    Create MemoryTools.h

---
+++ MemoryTools.h
@@ -0,0 +1,14 @@
+/*
+ * MemoryTools.h
+ *
+ *  Created on: Oct 22, 2021
+ *      Author: ASUS
+ */
+
+#ifndef MEMORYTOOLS_H_
+#define MEMORYTOOLS_H_
+
+INT16U MemChkSum(INT8U *startaddr, INT8U *endaddr);
+
+#endif /* MEMORYTOOLS_H_ */
+
